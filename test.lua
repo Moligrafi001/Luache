96c46947d61823e0c7d41267e1cfd8d2b514e981
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-12-10 05:38:09 -0300

    update test.lua

--- test.lua
+++ test.lua
@@ -1,33 +1,34 @@
-local LucheVitae = loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Luche-Vitae/refs/heads/main/Source/Library.lua"))() -- Load the Library
+local Luache = loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Luache/main/Source/Library.lua"))() -- Load the Library
 
-LucheVitae:Settings({ -- Load your settings
-  Service = "hallowhub", -- Your service identificator
-  DebugMode = true, -- Debug things in console
-  
-  KeySystem = { -- Delete this if you're not using the key system
-    GuiMode = true, -- Simple GUI to the key
-    SaveKey = true -- Save valid key
-  }
+Luache:Settings({ -- Load your settings
+  Service = "yourservice", -- Your service identificator
+  DebugMode = true -- Debug things in console
 })
+
+if not Luache:Implement("Check Banned") then return end -- Will stop the script if the user is banned
 
 -- Key System Functions
 local function ValidateKey(key) -- Your own custom function to check the key
-  if LucheVitae:AuthKey(key) == "valid" then -- If key is valid
-    print("Key is valid, loading...") -- Valid notification
-  elseif LucheVitae:AuthKey(key) == "expired" then -- If key expired
-    print("Key expired!") -- Expired notification
-  elseif LucheVitae:AuthKey(key) == "invalid" then -- If key is invalid
-    print("Key is invalid!") -- Invalid notification
-  else -- If authentication failed
-    print("Failed to check the key.") -- Failed to check key notification
-  end -- Add your own actions
+  local success, message = Luache:Check(key) -- Checks the key and store the results
+  
+  if success then -- success is a boolean: true / false
+    print("Valid!")
+  else
+    print("Invalid!")
+  end
+  
+  print(message) -- message is a 2nd arg sent from the check function telling you the reason for the result
+  
+  result success, result -- Returns the same as the Luache check function
 end
 local function GetKey() -- Your own custom function to get key
-  setclipboard(LucheVitae:GetKey()) -- Will copy the get key url
+  setclipboard(Luache:GetKey()) -- Will copy the get key url returned by Luache
 end
 
--- Implementation Functions
-LucheVitae:Implement("Log Statistic") -- Log this execution statistics
-LucheVitae:Implement("Check Banned") -- Check if the user is banned
-LucheVitae:Implement("Send webhook") -- Send webhook embed
-LucheVitae:Implement("Everything") -- Will do the same as the 3 above
+if ValidateKey("YOUR-KEY-HERE") then
+  Luache:Implement("Everything") -- Send webhook embed
+  
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))() -- Will execute infiniteyield
+else
+  GetKey() -- Will copy the getkey link if the key wasn't valid
+end
